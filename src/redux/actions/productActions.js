6b9a954df635c88ef97495f582066c4d55e1e9ea
Nandan Prasad <prasadnandan2595@gmail.com>
2024-06-26 14:38:09 +0530

    Add ProductDetailPage

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -13,3 +13,10 @@ export const selectedProducts = (product) => {
         payload: product
     };
 }
+
+
+export const removeSelectedProducts = () => {
+    return {
+        type: ActionTypes.REMOVE_SELECTED_PRODUCT,
+    };
+}
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
-import { productReducer } from "./productReducers";
+import { productReducer, selectProductReducer } from "./productReducers";
 
 export const reducers = combineReducers({
     allProducts: productReducer,
+    product: selectProductReducer
 })
